4bf61fd38ae8046f64c0c3918eaf4632437b423e
animevn <haanhth21@gmail.com>
2020-04-04 18:36:54 +0700

    add circle and color

--- src/Home.js
+++ src/Home.js
@@ -24,25 +24,28 @@ function Home() {
     canvas.height = height * ratio;
     canvas.style.width = `${width}px`;
     canvas.style.height = `${height}px`;
+
+    let requestId;
+    let i = 0;
     const render = ()=>{
       ctx.beginPath();
       ctx.fillStyle = "#ffbf00";
-      ctx.arc(canvas.width/4, canvas.height/4, canvas.width/4, 0, 2 * Math.PI);
+      ctx.arc(canvas.width/2, canvas.height/2,
+        (canvas.width/2) * Math.abs(Math.cos(i)), 0, 2 * Math.PI);
       ctx.fill();
+      i += 0.05;
+      requestId = requestAnimationFrame(render);
     };
 
-    requestAnimationFrame(render);
+    render();
+    return ()=> {
+      cancelAnimationFrame(requestId);
+    }
+
   });
 
   return (
-    <div className="container mt-3">
-
-      <canvas ref={canvasRef}
-              className="row col-xl-7 col-lg-8 col-md-10 col-sm-12 col-12 mx-auto my-3 px-0">
-
-      </canvas>
-
-    </div>
+    <canvas ref={canvasRef} style={{ width: '100px', height: '100px' }}/>
   )
 }
 
